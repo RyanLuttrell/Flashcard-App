608a9450f126e5a8e5f322f7c4f146dd2f814c7c
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-02 21:54:10 -0600

    Add routes and setup post for form submissions

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'pug');
 
@@ -13,6 +15,15 @@ app.get('/cards', (req, res) => {
     res.render('card', {prompt: "Who is buried in Grant's tomb?"});
 });
 
-app.listen(3000, () => {
+app.get('/hello', (req, res) => {
+    res.render('hello');
+})
+
+app.post('/hello', (req, res) => {
+    console.dir(req.body);
+    res.render('hello');
+})
+
+app.listen(3001, () => {
     console.log('The application is running on localhost:3000')
 });
