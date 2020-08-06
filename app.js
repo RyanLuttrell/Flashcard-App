261b77525af44ae7546f072823545ab12969184c
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-06 13:25:15 -0700

    Add error handling for 500 and 404

--- app.js
+++ app.js
@@ -10,13 +10,8 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.use((req, res, next) => {
-    console.log('Hello');
-    next();
-});
-
-app.use((req, res, next) => {
     console.log('World');
-    next()
+    next();
 });
 
 app.get('/', (req, res) => {
@@ -49,6 +44,18 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
     res.clearCookie('username');
     res.redirect('/hello')
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
