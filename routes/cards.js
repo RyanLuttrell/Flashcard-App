cae9dba6c6185b0837273413ee2077d148ac6e39
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-06 16:25:11 -0700

    Incorporate styles

--- routes/cards.js
+++ routes/cards.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data/flashcardData.json').data;
-const cards = data.cards;
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
 
-router.get('/', (req, res) => {
-    const numberOfCards = cards.length;
-    const flashcardId = Math.floor(Math.random()*numberOfCards);
-    res.redirect(`/cards/${flashcardId}`);
-})
+router.get( '/', ( req, res ) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect( `/cards/${flashcardId}` )
+});
 
 router.get('/:id', (req, res) => {
-    const side = req.query.side;
-    const id = req.params.id;
+    const { side } = req.query;
+    const { id } = req.params;
 
-    if (!side) {
-        res.redirect(`/cards/${id}?side=question`);
+    if ( !side ) {
+        return res.redirect(`/cards/${id}?side=question`);
     }
     const name = req.cookies.username;
     const text = cards[id][side];
-    const hint = cards[id].hint;
-
-    const templateData = {id, text, name};
+    const { hint } = cards[id];
     
-    if (side === 'question') {
-        templateData.hint = hint;
-        templateData.sideToShow = 'answer';
-        templateData.sideToShowDisplay = 'Answer';
-    } else if (side === 'answer') {
-        templateData.sideToShow = 'question';
-        templateData.sideToShowDisplay = 'Question'
+    const templateData = { id, text, name, side };
+
+    if ( side === 'question' ) {
+      templateData.hint = hint;
+      templateData.sideToShow = 'answer';
+      templateData.sideToShowDisplay = 'Answer';
+    } else if ( side === 'answer' ) {
+      templateData.sideToShow = 'question';
+      templateData.sideToShowDisplay = 'Question';
     }
 
     res.render('card', templateData);
